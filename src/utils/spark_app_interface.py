df0c9a064f264244f9a26b7f5a5ff91627db462a
John W <johnrwalsh34@gmail.com>
2024-12-09 11:19:04 -0700

    Ruff and mypy fixes

--- src/utils/spark_app_interface.py
+++ src/utils/spark_app_interface.py
@@ -1,47 +1,98 @@
-#import modules
-from pyspark import SparkContext
-from pyspark.sql import SQLContext
-from pyspark.sql import SparkSession
+"""Module For Building and Using Spark ML Models in the resume_app."""
 
-from pyspark.ml.feature import RegexTokenizer, StopWordsRemover, CountVectorizer
-from nltk.corpus import stopwords
+# Import modules
+from pyspark.sql import SQLContext, SparkSession
 from pyspark.ml import PipelineModel
 from pyspark.ml.classification import LogisticRegressionModel
 import pandas as pd
 
-#def function to create spark instance in app
-def create_spark_instance():
-    spark = SparkSession.builder.getOrCreate()#.appName("Emotion Classifier")
-    sqlContext = SQLContext(spark)
+
+# Def function to create spark instance in app
+def create_spark_instance() -> SQLContext:
+    """Create a Spark SQL Context for in-app use."""
+    spark = SparkSession.builder.appName("Emotion Classifier").getOrCreate()
+    sqlContext = SQLContext(spark.sparkContext)
     return sqlContext
 
-#def function to load lrModel into app environment, may need a cache wrapper
-def load_lrModel(model_path:str):
+
+# Def function to load lrModel into app environment, may need a cache wrapper
+def load_lrModel(model_path: str) -> LogisticRegressionModel:
+    """
+    Load LR Model into environment.
+
+    Input:
+    -----
+    model_path : Filepath containing serialized LR Model
+
+    Returns
+    -------
+    lrModel : Functional Instance of loaded Spark LR Model
+
+    """
     lrModel = LogisticRegressionModel.load(model_path)
     return lrModel
 
-#def function to load prefit transformation pipeline to allow for data ingestion into model
-def load_pipeline(pipeline_path:str):
+
+# Def function to load prefit transformation pipeline to allow for data ingestion into model
+def load_pipeline(pipeline_path: str) -> PipelineModel:
+    """
+    Load Spark Transformation Pipeline into environment.
+
+    Input:
+    -----
+    pipeline_path : Filepath containing serialized Spark Transformation Pipeline
+
+    Returns
+    -------
+    fitPipeline : Prefit Spark Transformation Pipeline.
+
+    """
     fitPipeline = PipelineModel.load(pipeline_path)
     return fitPipeline
 
-def classify_input(input_txt:str, sqlContext, fitPipeline, lrModel):
-    #define class label mapping to map model output
-    class_label_mapping = {'0' : 'sadness', '1' : 'joy', 
-                           '2' : 'love', '3' : 'anger', 
-                           '4' : 'fear', '5' : 'surprise'}
-    
-    #convert input into spark digestible format
-    inputDF = sqlContext.createDataFrame(pd.DataFrame({'text': [input_txt]}, columns = ['text']))
-    
-    #transform txt via fitPipeline
+
+def classify_input(
+    input_txt: str,
+    sqlContext: SQLContext,
+    fitPipeline: PipelineModel,
+    lrModel: LogisticRegressionModel,
+) -> str:
+    """
+    Accept str input, transform in proper shape, return LR Model output.
+
+    Input:
+    -----
+    input_txt   : User String Input to be classified
+    sqlContext  : SQL Context required for spark functionality
+    fitPipeline : Loaded Transformation Pipeline to ingest data
+    lrModel     : Loaded Text Classification Model
+
+    Returns
+    -------
+    classification label from numeric output of lrModel
+
+    """
+    # Define class label mapping to map model output
+    class_label_mapping = {
+        "0": "sadness",
+        "1": "joy",
+        "2": "love",
+        "3": "anger",
+        "4": "fear",
+        "5": "surprise",
+    }
+
+    # Convert input into spark digestible format
+    inputDF = sqlContext.createDataFrame(
+        pd.DataFrame({"text": [input_txt]}, columns=["text"])
+    )
+
+    # Transform txt via fitPipeline
     transformed_txt = fitPipeline.transform(inputDF)
-    
-    #prediction text class label via lrModel and extract prediction
-    input_pred = int(lrModel.transform(transformed_txt).toPandas()['prediction'].values[0])
+
+    # Prediction text class label via lrModel and extract prediction
+    input_pred = int(
+        lrModel.transform(transformed_txt).toPandas()["prediction"].values[0]
+    )
 
     return class_label_mapping[str(input_pred)]
-
-    
-    
-
