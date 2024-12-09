e9ca2d0621dbb2fac1bb79ea45fba6ef8676b865
John W <johnrwalsh34@gmail.com>
2024-12-09 11:18:09 -0700

    Add documentation to dtypes

--- src/utils/AppDataStructs/section_descriptions.py
+++ src/utils/AppDataStructs/section_descriptions.py
@@ -1,153 +1,154 @@
 """File containing data structures used in section description population."""
+
 from typing import TypedDict, Dict
 
 reference_markdown: str = """
                 ## Sections
-            
+
                 [Education](#education)
-            
+
                 [Internship](#internship)
-            
+
                 [Skills](#skills)
-            
+
                 [Capstone Project for The Data Incubator](#ingredient-identifier)
-            
+
                 [pySpark Text Classification Model](#pyspark-text-classification-model)
 
                 [How I Built This](#how-i-built-this)
 
                 [Contact Info](#contact-info)
-            
+
                 """
 
 about_me_description: str = """
-                
+
         I am a Data Scientist and Python Developer who is enthusiastic about learning new technologies
-        and adapting to innovations in the field. 
+        and adapting to innovations in the field.
 
-        All of the facets of machine learning are vital arrows in the 
-        quiver of business problem solving. Data is a tool drastically 
-        improving the world and I am thrilled to participate in bringing 
+        All of the facets of machine learning are vital arrows in the
+        quiver of business problem solving. Data is a tool drastically
+        improving the world and I am thrilled to participate in bringing
         data to life.
-                        
-        With firm belief in hard work, I persistently strive to produce meaningful, 
-        thorough, and reproducible results built from an adequate mathematical
-        foundation. 
 
-        Furthermore, I hold communication as essential in the data science 
+        With firm belief in hard work, I persistently strive to produce meaningful,
+        thorough, and reproducible results built from an adequate mathematical
+        foundation.
+
+        Furthermore, I hold communication as essential in the data science
         process and there is little value in improperly communicated insights derived from data.
 
-        Beyond my interests as a data scientist I often find myself trail running or travelling when the opportunities arise. 
-                
+        Beyond my interests as a data scientist I often find myself trail running or travelling when the opportunities arise.
+
         """
 
 tutoring_description: str = """
-        Courses Tutored: Matrix Theory, Introduction to Statistics, 
+        Courses Tutored: Matrix Theory, Introduction to Statistics,
         Calculus 1,2 & 3, and Differential Equations
 
-        Found success by identifying real world applications of the pertinant mathematics 
-        when tutoring students with nontechnical backgrounds. 
+        Found success by identifying real world applications of the pertinant mathematics
+        when tutoring students with nontechnical backgrounds.
 
         Timeframe: Jan. 2021 - May 2023
         """
 
 comp_chem_descrption: str = """
-        Performed least squares optimization (analytically) using SVD in a Linux environment to project data points 
+        Performed least squares optimization (analytically) using SVD in a Linux environment to project data points
         onto a best-fit plane in 3-D. As a result, enabled better assessment of molecular interactions in the dynamic model.
 
         Timeframe: May 2021 - Aug. 2021
         """
 
 thesis_description: str = """
-            It was a great opportunity to work on a project from start to finish. 
-            My project entailed retrieving the data, anonymizing, cleaning, analyzing, processing, 
+            It was a great opportunity to work on a project from start to finish.
+            My project entailed retrieving the data, anonymizing, cleaning, analyzing, processing,
             and modelling the data in a manner that was generalizable to other company assets.
-            One particularly exciting facet of the project is the transparency (explainability) 
-            of the insights generated, an important feature considering strict documentation 
-            is required to explain decisions guiding users of a model of this nature. 
+            One particularly exciting facet of the project is the transparency (explainability)
+            of the insights generated, an important feature considering strict documentation
+            is required to explain decisions guiding users of a model of this nature.
         """
 
 # Capstone Descriptions
 capstone_data_and_motivation: str = """
-             
+
             USDA Branded Food Items Dataset contains all food items registered with the USDA. The dataset
             also contains informaiton about the ingredients, description, brand owner, and parent company selling the items.
 
             This dataset is > 2GB and contains more than 3,568,000 food and beverage products' label information.
 
-            An application was built that assists consumers in finding food items registered 
+            An application was built that assists consumers in finding food items registered
             with the USDA that match criteria according to their dietary preferences.
-                
+
             The motivation for this project came from encountering the reaction mechanism of the preservative
             Sodium Benzoate and Ascorbic Acid (Vitamin C). Under conditions favoring homolysis (high temperatures or
-            sun exposure) benzene is a byproduct; albiet, often in extremely small quantities. Still, I was concerned 
+            sun exposure) benzene is a byproduct; albiet, often in extremely small quantities. Still, I was concerned
             with the potential presence of a carcinogen floating around in my orange juice. So I thought an application
             that empowered users to make more informed decisions about items they consume would be a useful tool.
-                
+
             """
 
 capstone_cleaning_and_processing: str = """
 
-            As with most data in the real world. Some cleaning needed to be done to ensure consistency 
-            with the desired functionality. To accomplish this regular expressions were utilized to 
-            standardize and parse comma separated strings into python lists. 
+            As with most data in the real world. Some cleaning needed to be done to ensure consistency
+            with the desired functionality. To accomplish this regular expressions were utilized to
+            standardize and parse comma separated strings into python lists.
 
             Many ingredient labels contain some rendition of a tag like 'Contains 0.5% or less of the following'.
             However, given the variety of food products and producers, this tag is far from standardized in the dataset.
-            Thus regex was a great tool to generalize the above tag to account for edge cases like contain(s), (x)%, 
-            less than, less, or less, or less than, etc. 
+            Thus regex was a great tool to generalize the above tag to account for edge cases like contain(s), (x)%,
+            less than, less, or less, or less than, etc.
 
             Another textual behavior in need of wrangling were descriptive modifiers before and after ingredients.
-            These modifiers were of the form: 'as an anti-caking agent', 'to preserve freshness', 'for color', 'used for', etc. 
-            Many of these occurences were identified and added to a list which was scanned for each item in the dataset and removed if 
-            they were found in the ingredient list. 
+            These modifiers were of the form: 'as an anti-caking agent', 'to preserve freshness', 'for color', 'used for', etc.
+            Many of these occurences were identified and added to a list which was scanned for each item in the dataset and removed if
+            they were found in the ingredient list.
 
             Parsing the data was a very important step so ample time was spent covering as many different forms of data impurity as
             possible to ensure noningredient words would not be passed as ingredients to the database.
-            
+
             """
 
 capstone_database_management_and_interface: str = """
             To efficiently access the processed data in an application, it was stored in a PostgresSQL database. Aiven was used to
-            host this database. Then to establish connections and perform in-app querying, sqlalchemy was used to pythonically 
-            execute SQL queries within the database. 
-            
+            host this database. Then to establish connections and perform in-app querying, sqlalchemy was used to pythonically
+            execute SQL queries within the database.
+
             """
 
 capstone_display_perfect_matches: str = """
             Once queried, the data was displayed using a custom hierarichally directed acyclic graph built in plotly.
-            Although this process was slightly tedious, the functionality and interactivity of the plotly figure 
-            generated proved worth the effort. One issue encountered during the developement of the visualization was 
-            dealing with queries returning large result sets. 
-            
-            This issue was resolved with a rendition of an alternating sequence, utilized to compute the horizontal 
+            Although this process was slightly tedious, the functionality and interactivity of the plotly figure
+            generated proved worth the effort. One issue encountered during the developement of the visualization was
+            dealing with queries returning large result sets.
+
+            This issue was resolved with a rendition of an alternating sequence, utilized to compute the horizontal
             distribution of nodes on the graph. Ensuring spacing would account for various items per brand whilst remaining
             independent of the number of nodes on the graph.
             """
 
 capstone_data_source: str = """
     Data Source For Ingredient Identifier:
-    
-    U.S. Department of Agriculture, Agricultural Research Service. 
+
+    U.S. Department of Agriculture, Agricultural Research Service.
     FoodData Central, 2023. fdc.nal.usda.gov.
-    
+
     """
 
 # PySpark Classifier Descriptions
-pyspark_build_description: str = """        
+pyspark_build_description: str = """
         Building this model a balance was acheived on overfitting based on single words. It has proven quite easy to trick the model
-        with oxymoronic input. The training data also elucidates the complexity of language and the dangers of 
+        with oxymoronic input. The training data also elucidates the complexity of language and the dangers of
         restricting human behavior to oversimplified descriptions (speech only being categorically confined to 6 emotions).
         """
 
 pyspark_model_performance: str = """
-            The model performed with 90% accuracy on the training data and the most important parameter was document frequency count in the 
-        processing step. 
-        
-        This was a great reminder of the age old adage 'garbage in garbage out' because despite pulling all of the levers of 
+            The model performed with 90% accuracy on the training data and the most important parameter was document frequency count in the
+        processing step.
+
+        This was a great reminder of the age old adage 'garbage in garbage out' because despite pulling all of the levers of
         the logistic regression model (which performed better than 4 other models), the accuracy was limited by how the text was preprocessed.
-        
-        Another apparent shortcoming of the model is the inability to classify surprise without the presence of explicit surprise 
+
+        Another apparent shortcoming of the model is the inability to classify surprise without the presence of explicit surprise
         related keywords in the text. This is likely due to the descrepency between the number of training labels associated with surprise
         which had approximately 15,000 labels compared to all other labels with at least four times as many for training.
             """
@@ -156,22 +157,22 @@ pyspark_hyperparams: str = """
             The models hyperpameters were tuned using pySpark's CrossValidator with ClassificationEvaluator.
 
             As stated above, the greatest improvement in model accuracy was achieved by increasing document frequency count during
-            the text vectorization step. 
-            
+            the text vectorization step.
+
             """
 
 pyspark_example_outputs: str = """
             The model sometimes performs surprisingly well with more abstract emotional expressions,
-            here are some examples of this abstract metaphoric classification: 
-        
+            here are some examples of this abstract metaphoric classification:
+
         "That ship has left the harbour" = sadness
-        
+
         "His blood boiled" = anger
-        
+
         "You are my number 1" = joy
-        
+
         "Watching that whale warmed my heart" = love
-        
+
         "I almost soiled myself when the leopard ran at me" = fear
 
         "Is it possible to do that" = surprise
@@ -184,12 +185,12 @@ pyspark_input_guide: str = """
 
 pyspark_data_source: str = """
 
-        Data Source For Training Spark Model: 
-        
+        Data Source For Training Spark Model:
+
             @inproceedings{saravia-etal-2018-carer,\n
             title = "{CARER}: Contextualized Affect Representations for Emotion Recognition",\n
-            author = "Saravia, Elvis  and Liu, Hsien-Chi Toby  and Huang, Yen-Hao 
-                        and Wu, Junlin  and Chen, Yi-Shin",\n 
+            author = "Saravia, Elvis  and Liu, Hsien-Chi Toby  and Huang, Yen-Hao
+                        and Wu, Junlin  and Chen, Yi-Shin",\n
             booktitle = "Proceedings of the 2018 Conference on Empirical Methods in Natural Language Processing",\n
             month = oct # "-" # nov, year = "2018",\n
             address = "Brussels, Belgium",\n
@@ -197,38 +198,41 @@ pyspark_data_source: str = """
             url = "https://www.aclweb.org/anthology/D18-1404",\n
             doi = "10.18653/v1/D18-1404",\n
             pages = "3687--3697",\n
-            
-            abstract = "Emotions are expressed in nuanced ways, which varies by collective or individual experiences, knowledge, and beliefs. 
-            Therefore, to understand emotion, as conveyed through text, a robust mechanism capable of capturing and modeling different 
-            linguistic nuances and phenomena is needed. We propose a semi-supervised, graph-based algorithm to produce rich structural 
-            descriptors which serve as the building blocks for constructing contextualized affect representations from text. The pattern 
-            based representations are further enriched with word embeddings and evaluated through several emotion recognition tasks. Our 
+
+            abstract = "Emotions are expressed in nuanced ways, which varies by collective or individual experiences, knowledge, and beliefs.
+            Therefore, to understand emotion, as conveyed through text, a robust mechanism capable of capturing and modeling different
+            linguistic nuances and phenomena is needed. We propose a semi-supervised, graph-based algorithm to produce rich structural
+            descriptors which serve as the building blocks for constructing contextualized affect representations from text. The pattern
+            based representations are further enriched with word embeddings and evaluated through several emotion recognition tasks. Our
             experimental results demonstrate that the proposed method outperforms state-of-the-art techniques on emotion recognition tasks.",}
-        
+
         """
 
 how_i_built_this: str = """
         To checkout the python code used to build this app, click the github logo in the top right corner.
-        
+
         For incorporating the pySpark classification model, Scala and Java are needed in the application environment.
         Thankfully streamlit allows for additional non-python packakes to be specified in a 'packages.txt' file.
-        
+
         The autoscroll/anchor feature in the 'Sections' container is a nice trick streamlit inherited from markdown.
-        
+
         All figures in the app were built using Plotly. Plotly is a personal favorite for high-level visualizations because I
         have yet to encounter a visualization that cannot be built (often concisely)
         using either plotly express or the more robust figure objects. It also has nice features for creating animations
         like the one found in the [Internship](#internship) section.
-        
+
         Another great feature of streamlit is the ability to add the download button for any file in the environment.
-        In a data science context this could even be used to train a model or configure a visualization and then allow for the user 
+        In a data science context this could even be used to train a model or configure a visualization and then allow for the user
         to download that object directly from the application.
-        
+
         """
 
-#Create TypedDict class to store section description Data
+
+# Create TypedDict class to store section description Data
 ## When New Sections are add this typing structure needs to be updated as well
 class SectionDescriptions(TypedDict):
+    """Typed Dictionary Holding Section Descriptions."""
+
     reference_section: str
     about_me: str
     tutoring: str
@@ -237,6 +241,7 @@ class SectionDescriptions(TypedDict):
     capstone: Dict[str, str]
     pyspark: Dict[str, str]
     how_i_built_this: str
+
 
 # Dictionary Containing all text sections for easy import and readibility
 section_descriptions: SectionDescriptions = {
